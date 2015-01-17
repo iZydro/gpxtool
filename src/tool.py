701416393c16e8fbedefcf3e699cee134b289559
Isidro Gilabert <isidrogilabert@gmail.com>
2015-01-17 18:45:36 +0100

    Refactors

--- src/tool.py
+++ src/tool.py
@@ -6,14 +6,14 @@ import os
 import urllib
 
 import globalmaptiles
-from Tkinter import Tk
 
-class simpleapp_tk(Tkinter.Tk):
+
+class GPXTool(Tkinter.Tk):
     
     image = None
     background = None
     panning = False
-    coords = []
+    coordinates = []
     moved = []
     
     canvas_width = 256
@@ -21,143 +21,144 @@ class simpleapp_tk(Tkinter.Tk):
 
     mercator = None
     
-    scales = { 17:(100/110) }
-    
-    def __init__(self,parent):
-        Tkinter.Tk.__init__(self,parent)
+    def __init__(self, parent):
+        Tkinter.Tk.__init__(self, parent)
         self.parent = parent
-        self.initialize()
-        self.bind("<Key>", self.key)
-    
 
-    def initialize(self):
         self.grid()
 
+        self.bind("<Key>", self.key)
+
         self.entryVariable = Tkinter.StringVar()
-        self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
-        self.entry.grid(column=0,row=0,sticky='EW')
-        self.entry.bind("<Return>", self.OnPressEnter)
+        self.entry = Tkinter.Entry(self, textvariable=self.entryVariable)
+        self.entry.grid(column=0, row=0, sticky='EW')
+        self.entry.bind("<Return>", self.on_press_enter)
         self.entryVariable.set("Hello World")
 
-        button = Tkinter.Button(self,text="Click me !",
-                                command=self.OnButtonClick)
-        button.grid(column=1,row=0)
+        button = Tkinter.Button(self, text="Click me !",
+                                command=self.on_button_click)
+        button.grid(column=1, row=0)
 
         self.labelVariable = Tkinter.StringVar()
-        label = Tkinter.Label(self,textvariable=self.labelVariable,
-                              anchor="w",fg="white",bg="blue")
-        label.grid(column=0,row=1,columnspan=2,sticky='EW')
+        label = Tkinter.Label(self, textvariable=self.labelVariable,
+                              anchor="w", fg="white", bg="blue")
+        label.grid(column=0, row=1, columnspan=2, sticky='EW')
         
-        self.coords = (41.406271,2.212483,17)
-        self.entryVariable.set(str(self.coords[0]) + "," + str(self.coords[1]) + "," + str(self.coords[2]))
+        self.coordinates = (41.406271, 2.212483, 17)
+        self.set_entry_variable(self.coordinates)
 
-        self.canvas_width = 500
-        self.canvas_height = 500
+        self.canvas_width = 640
+        self.canvas_height = 640
         self.background = Tkinter.Canvas(self, width=self.canvas_width, height=self.canvas_height)
-        self.background.grid(column=0,row=2,columnspan=2)
-        self.UpdateCoords(self.entryVariable.get().split(","))
-        
+        self.background.grid(column=0, row=2, columnspan=2)
+        self.update_coordinates(self.entryVariable.get().split(","))
+
         self.background.create_rectangle(100, 100, 20, 20, fill="#ff0000")
         
         self.background.bind("<ButtonPress-1>", self.press_button)
         self.background.bind("<ButtonRelease-1>", self.release_button)
         self.background.bind("<B1-Motion>", self.motion_button)
 
-        self.grid_columnconfigure(0,weight=1)
-        self.resizable(True,False)
+        self.grid_columnconfigure(0, weight=1)
+        self.resizable(True, False)
         self.update()
         self.geometry(self.geometry())       
-        #self.entry.focus_set()
-        #self.entry.selection_range(0, Tkinter.END)
-        
-        self.coords = [0, 0, 0]
+
+        self.coordinates = [0, 0, 0]
         self.moved = [0, 0]
 
         self.mercator = globalmaptiles.GlobalMercator()
 
+    def set_entry_variable(self, values_array):
+        value = str(values_array[0]) + "," + str(values_array[1]) + "," + str(values_array[2])
+        self.entryVariable.set(value)
+
     def key(self, event):
         print ("pressed", repr(event.char))
         if event.char == "+":
-            self.ChangeZoom(1)
+            self.change_zoom(1)
         elif event.char == "-":
-            self.ChangeZoom(-1)
+            self.change_zoom(-1)
     
     def press_button(self, event):
         print ("clicked at", event.x, event.y)
-        self.coords[0] = event.y
-        self.coords[1] = event.x
+        self.coordinates[0] = event.y
+        self.coordinates[1] = event.x
         self.background.focus_set()
 
     def motion_button(self, event):
         print ("motion at", event.x, event.y)
-        self.moved[0] = event.y - self.coords[0]
-        self.moved[1] = - (event.x - self.coords[1])
+        self.moved[0] = event.y - self.coordinates[0]
+        self.moved[1] = - (event.x - self.coordinates[1])
         self.background.delete(Tkinter.ALL)
-        self.background.create_image(250 - self.moved[1], 250 + self.moved[0], image=self.image)
+        self.background.create_image(self.canvas_width / 2 - self.moved[1],
+                                     self.canvas_height / 2 + self.moved[0],
+                                     image=self.image)
 
     def release_button(self, event):
         print ("clicked at", event.x, event.y)
-        self.moved[0] = event.y - self.coords[0]
-        self.moved[1] = - (event.x - self.coords[1])
+        self.moved[0] = event.y - self.coordinates[0]
+        self.moved[1] = - (event.x - self.coordinates[1])
 
         print (self.moved)
 
-        old_coords = self.entryVariable.get().split(",")
-        print(old_coords)
-        lat = float(old_coords[0])
-        lon = float(old_coords[1])
-        tz = float(old_coords[2])
-        mx, my = self.mercator.LatLonToMeters( lat, lon )
+        old_coordinates = self.entryVariable.get().split(",")
+        print(old_coordinates)
+        lat = float(old_coordinates[0])
+        lon = float(old_coordinates[1])
+        tz = float(old_coordinates[2])
+        mx, my = self.mercator.LatLonToMeters(lat, lon)
         print ("Spherical Mercator (ESPG:900913) coordinates for lat/lon: ")
         print (mx, my)
-        tminx, tminy = self.mercator.MetersToTile(mx, my, tz )
-        wgsbounds = self.mercator.TileLatLonBounds(tminx, tminy, tz)
+        current_tile_x, current_tile_y = self.mercator.MetersToTile(mx, my, tz)
+        wgs_bounds = self.mercator.TileLatLonBounds(current_tile_x, current_tile_y, tz)
 
-        scale_x = (wgsbounds[2] - wgsbounds[0]) / 256 # self.canvas_width
-        scale_y = (wgsbounds[3] - wgsbounds[1]) / 256 # self.canvas_height
+        scale_x = (wgs_bounds[2] - wgs_bounds[0]) / 256
+        scale_y = (wgs_bounds[3] - wgs_bounds[1]) / 256
 
-        old_coords = self.entryVariable.get().split(",")
-        new_coords = [0, 0, 0]
-        new_coords[0] = float(old_coords[0]) + float(self.moved[0]) * scale_x
-        new_coords[1] = float(old_coords[1]) + float(self.moved[1]) * scale_y
-        new_coords[2] = old_coords[2]
-        self.UpdateCoords(new_coords)
-        self.entryVariable.set(str(new_coords[0]) + "," + str(new_coords[1]) + "," + str(new_coords[2]))
+        old_coordinates = self.entryVariable.get().split(",")
+        new_coordinates = [0, 0, 0]
+        new_coordinates[0] = float(old_coordinates[0]) + float(self.moved[0]) * scale_x
+        new_coordinates[1] = float(old_coordinates[1]) + float(self.moved[1]) * scale_y
+        new_coordinates[2] = old_coordinates[2]
+        self.update_coordinates(new_coordinates)
+        self.set_entry_variable(new_coordinates)
+#        self.entryVariable.set(str(new_coordinates[0]) + "," + str(new_coordinates[1]) + "," + str(new_coordinates[2]))
 
-# 0.0000160 coords, 100 pixels
-        
-    def UpdateCoords(self, coords):
-        #self.entry.focus_set()
-        #self.entry.selection_range(0, Tkinter.END)
-        
-        #coords_text = self.entryVariable.get().split(",")
-        request = "http://maps.google.com/maps/api/staticmap?center=" + str(coords[0]) + "," + str(coords[1]) + "&zoom=" + str(coords[2]) +"&size=500x500&format=gif&maptype=hybrid&sensor=false"
+    def update_coordinates(self, coordinates):
+
+        request = "http://maps.google.com/maps/api/staticmap?center="
+        request += str(coordinates[0]) + "," + str(coordinates[1]) + "&zoom=" + str(coordinates[2])
+        request += "&size=" + str(self.canvas_width)
+        request += "x"
+        request += str(self.canvas_height)
+        request += "&format=gif&maptype=hybrid&sensor=false"
+        request += "&key=AIzaSyBOP8yEyxoR2jYdYBf4th6hSgdaUeWsBx0"
         print(request)
         urllib.urlretrieve(request, "caca.gif")
         image = Tkinter.PhotoImage(file="./caca.gif")
         self.image = image
-        self.background.create_image(250, 250, image=self.image)
+        self.background.create_image(self.canvas_width / 2, self.canvas_height / 2, image=self.image)
 
-    def ChangeZoom(self, offset):
-        new_coords = self.entryVariable.get().split(",")
-        zoom = int(new_coords[2])
+    def change_zoom(self, offset):
+        new_coordinates = self.entryVariable.get().split(",")
+        zoom = int(new_coordinates[2])
         zoom += offset
-        new_coords[2] = zoom
-        self.UpdateCoords(new_coords)
-        self.entryVariable.set(str(new_coords[0]) + "," + str(new_coords[1]) + "," + str(new_coords[2]))
+        new_coordinates[2] = zoom
+        self.update_coordinates(new_coordinates)
+        self.entryVariable.set(str(new_coordinates[0]) + "," + str(new_coordinates[1]) + "," + str(new_coordinates[2]))
 
-        
-    def OnButtonClick(self):
-        self.labelVariable.set( self.entryVariable.get()+" (You clicked the button)" )
-        self.UpdateCoords(self.entryVariable.get().split(","))
+    def on_button_click(self):
+        self.labelVariable.set(self.entryVariable.get() + " (You clicked the button)")
+        self.update_coordinates(self.entryVariable.get().split(","))
         print("Focus get " + self.focus_get())
 
-    def OnPressEnter(self,event):
-        self.labelVariable.set( self.entryVariable.get()+" (You pressed Enter)" )
-        self.UpdateCoords(self.entryVariable.get().split(","))
+    def on_press_enter(self, event):
+        self.labelVariable.set(self.entryVariable.get() + " (You pressed Enter)" + event.char)
+        self.update_coordinates(self.entryVariable.get().split(","))
 
 if __name__ == "__main__":
-    app = simpleapp_tk(None)
+    app = GPXTool(None)
     os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
     app.title('Zydro GPX tool')
     
